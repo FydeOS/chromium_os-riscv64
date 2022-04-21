4f06d2bbf017f445778b996cb80b4354ef227993
Su Yue <glass@fydeos.io>
2022-04-21 12:54:21 +0800

    tweak setup_board.sh
    Signed-off-by: Su Yue <glass@fydeos.io>

--- bin/setup_board.sh
+++ bin/setup_board.sh
@@ -2,6 +2,7 @@
 CROSSDEV_VERSION="crossdev-20211121"
 CROSSDEV=/usr/local/portage/crossdev/cross-riscv64-cros-linux-gnu
 CHROME_OVERLAY=/mnt/host/source/src/private-overlays/project-arch-riscv
+gcc11_ebuild=${CROSSDEV}/gcc/gcc-11.2.0.ebuild
 
 LINK_EBUILDS=(
   "sys-devel/binutils"
@@ -37,6 +38,12 @@ need_build_binutils-libs() {
   [ -z "$(find /usr/lib* -name libctf.so.0 -exec readelf -V {} + |grep LIBCTF_1.1)"  ]
 }
 
+
+need_build_glibc() {
+  [ -z "$(which riscv64-cros-linux-gnu-gcc 2> /dev/null)" ] && return 0
+  [[ "$(riscv64-cros-linux-gnu-gcc -v 2>&1 | grep "gcc version" | cut -d " " -f3 | cut -d '.' -f 1)" == "10" ]]
+}
+
 build_crossdev() {
   sudo emerge sys-devel/crossdev::chromeos-overlay
 }
@@ -61,16 +68,22 @@ build_binutils-libs() {
   sudo emerge binutils-libs::chromeos-overlay
 }
 
+build_glibc() {
+  sudo sed -i 's/KEYWORDS.*/KEYWORDS="~*"/g' ${gcc11_ebuild}
+  sudo emerge cross-riscv64-cros-linux-gnu/glibc
+  sudo sed -i 's/KEYWORDS.*/KEYWORDS="*"/g' ${gcc11_ebuild}
+}
+
 check_host_dep() {
   need_build_binutils-libs && build_binutils-libs
   need_build_crossdev && build_crossdev
-  need_build_qemu && build_qemu
+
   need_build_llvm && build_llvm
   need_build_rust && build_rust
 }
 
 create_links() {
-  [ -d ${CROSSDEV} ] || mkdir -p ${CROSSDEV}
+  [ -d ${CROSSDEV} ] || sudo mkdir -p ${CROSSDEV}
   for pkg in ${LINK_EBUILDS[@]}; do
     sudo ln -sf $CHROME_OVERLAY/$pkg ${CROSSDEV}/
   done
@@ -91,10 +104,16 @@ main() {
     echo "You must input board name with '--board=[board_name]'"
     exit 1
   fi
+
   create_links
   check_host_dep
+  #expect to fail
+  setup_board $@
+  # gcc11 needs glibc
+  need_build_glibc && build_glibc
   setup_board --nousepkg $@
   emerge-$board virtual/prepare-to-build-packages
+  need_build_qemu && build_qemu
 }
 
 main $@
